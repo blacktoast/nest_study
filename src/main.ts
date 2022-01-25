7e361a8ac3956f69693e1510fe49f596b6a721ec
blacktoast <dbstjd100@naver.com>
2022-01-25 08:27:48 +0000

    update :  readme 변경

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const prefix = '/api/';
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix(prefix);
+
   await app.listen(3000);
 }
 bootstrap();
