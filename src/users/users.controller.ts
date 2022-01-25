546caeeda5b45489944b79733f06a4150088dfb7
blacktoast <dbstjd100@naver.com>
2022-01-25 08:22:07 +0000

    feat: login 프로바이더 생성중

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,13 +2,16 @@ import { VerifyEmailDto } from './dto/verify-email.dto';
 import { CreateUserDto } from './dto/CreateUserDto';
 import { Controller, Post, Body, Query, Param, Get } from '@nestjs/common';
 import { UserLoginDto } from './dto/user-login.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private usersService: UsersService) {}
   @Post()
   createUser(@Body() createUserDto: CreateUserDto) {
-    const { name, email, password } = createUserDto;
+    const { name, password } = createUserDto;
     console.log(createUserDto);
+    return this.usersService;
   }
 
   @Post('/email-verify/')
@@ -17,8 +20,8 @@ export class UsersController {
   }
   @Post('/login')
   async login(@Body() dto: UserLoginDto): Promise<string> {
-    console.log(dto);
-    return;
+    const { name, password } = dto;
+    return this.usersService.login(name, password);
   }
 
   @Get('/:id')
---
+++ src/users/users.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@nestjs/common';
+
+const LoginKey =
+  '127AA49DAD7BEEC28DE548AF8968F071F361B69743629844E4F33A183A4A1C6D';
+
+@Injectable()
+export class UsersService {
+  async createUser(name: string, password: string) {
+    await this.checkUserExists(name);
+
+    await this.saveUser(name, password);
+  }
+
+  private checkUserExists(email: string) {
+    return false; // TODO: DB 연동 후 구현
+  }
+
+  private saveUser(name: string, password: string) {
+    return; // TODO: DB 연동 후 구현
+  }
+
+  l;
+  async login(name, password): Promise<string> {
+    // name  password가진 유저가 있는지 db에서 확인
+    // jwt 발급
+
+    throw new Error('not implement method');
+  }
+
+  async userInfo(userId: string) {
+    throw new Error('Method not implemented.');
+  }
+}
